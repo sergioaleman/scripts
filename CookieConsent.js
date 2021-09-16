17d8fa98228915c970645e5d9e2174d6174c2b7d
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 00:16:45 +0000

    Create CookieConsent.js
     <!-- Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
    <script type="text/javascript">
        window.cookieconsent_options = {"message":"Utilizamos cookies para   asegurar que damos la mejor   experiencia al usuario en   nuestro sitio web. Si continúa   utilizando este sitio asumiremos   que está de acuerdo.","dismiss":"ESTOY DE ACUERDO","learnMore":"Lee más información detallada","link":"http://www.infoudo.com.ve/politica-de-cookies","theme":"light-bottom"};
    </script>
    
    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
    <!-- End Cookie Consent plugin -->

---
+++ CookieConsent.js
@@ -0,0 +1,6 @@
+
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":"Utilizamos cookies para   asegurar que damos la mejor   experiencia al usuario en   nuestro sitio web. Si continúa   utilizando este sitio asumiremos   que está de acuerdo.","dismiss":"ESTOY DE ACUERDO","learnMore":"Lee más información detallada","link":"http://www.infoudo.com.ve/politica-de-cookies","theme":"light-bottom"};
+</script>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
